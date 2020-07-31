5b41171591bd2e49cc44621e3598e80f7f639a20
Michael Jester <m.jester.93@gmail.com>
2020-07-31 03:39:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,5 +17,7 @@ const Calculator = {
   add: function(num1, num2) {
     return num1 + num2;
   },
-  subtract: (num1, num2) => {return num1 + num2};
+  subtract: (num1, num2) => {
+    return num1 + num2
+  };
 }
