e471346bbbfaf5faf53191f0c9b39ee8a488d613
Michael Jester <m.jester.93@gmail.com>
2020-07-31 03:35:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@
 const saturdayFun = (activity="roller-skate") => {
   return `This Saturday, I want to ${activity}!`
 }
+
+const mondayWork = (activity="go to the office") => {
+  return `This Monday, I will ${activity}.`
+}
